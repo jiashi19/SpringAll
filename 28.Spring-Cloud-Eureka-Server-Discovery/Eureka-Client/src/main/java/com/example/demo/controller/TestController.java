32c70051137a025328da28b222badacc1ca3344e
jiashi <jiashi19@outlook.com>
2024-11-07 19:19:25 +0800

    新版本问题修复，DiscoveryClient替换成Registration

--- 28.Spring-Cloud-Eureka-Server-Discovery/Eureka-Client/src/main/java/com/example/demo/controller/TestController.java
+++ 28.Spring-Cloud-Eureka-Server-Discovery/Eureka-Client/src/main/java/com/example/demo/controller/TestController.java
@@ -3,8 +3,9 @@ package com.example.demo.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.serviceregistry.Registration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,14 +17,17 @@ public class TestController {
     @Autowired
     private DiscoveryClient client;
 
-//    @GetMapping("/info")
-//    public String info() {
-//        @SuppressWarnings("deprecation")
-//        ServiceInstance instance = client.getLocalServiceInstance();
-//        String info = "host：" + instance.getHost() + "，service_id：" + instance.getServiceId();
-//        log.info(info);
-//        return info;
-//    }
+    @Qualifier("eurekaRegistration")
+    @Autowired
+    private Registration registration;
+
+    @GetMapping("/info")
+    public String info() {
+        @SuppressWarnings("deprecation")
+        String info = "host：" + registration.getHost() + "，service_id：" + registration.getServiceId();
+        log.info(info);
+        return info;
+    }
 
     @GetMapping("/hello")
     public String hello() {
